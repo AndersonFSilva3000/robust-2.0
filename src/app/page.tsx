15e21198423b60638280049394b6aa8f1719a66e
Anderson F. Silva <www.anderson-ferdnand07@hotmai.com>
2023-12-09 20:41:59 -0300

    feat: initial settings

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Hello Wolrd</h1>
+      <h2>Welcome to my website</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde incidunt,
+        fugiat tempora dignissimos sed beatae autem quibusdam tempore nulla
+        atque dolor optio corrupti assumenda mollitia voluptate voluptatum
+        similique. Aspernatur, minima.
+      </p>
+      <p>teste 2</p>
+    </>
+  )
+}
